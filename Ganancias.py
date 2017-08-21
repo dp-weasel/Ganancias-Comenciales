d68169bf14f55f701503ca26ff5dcb0b8419cc1f
Weasel <luis_rph@openmailbox.org>
2017-08-21 04:09:02 -0300

    Addeded function anotations

--- Ganancias.py
+++ Ganancias.py
@@ -1,12 +1,12 @@
 # -*- encoding: utf-8 -*-
 
-def Calculo(cantidad, precio):
+def Calculo(cantidad: int, precio: float):
   total = 0
   for x in range(0, len(cantidad)):
     total = total + (cantidad[x] * precio[x])
   return total
 
-def Porcentaje(precio_compra, precio_venta):
+def Porcentaje(precio_compra: float, precio_venta: float):
   valor = ((precio_venta-precio_compra)*100)/precio_compra
   return valor
 
@@ -24,10 +24,10 @@ while opcion != 'n':
   valor = int(input("Cantidad del producto: "))
   cantidad.append(valor)
 
-  valor = int(input("Precio unitario del producto: "))
+  valor = float(input("Precio unitario del producto: "))
   precio_compra.append(valor)
 
-  valor = int(input("Precio de venta del producto: "))
+  valor = float(input("Precio de venta del producto: "))
   precio_venta.append(valor)
 
   opcion = input("¿Desea ingresar otro producto? (S/n): ")
