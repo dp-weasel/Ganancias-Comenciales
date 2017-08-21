7cfed3830a4d829802bd8da34ba50d6e2e34dd8a
Weasel <luis_rph@openmailbox.org>
2017-08-21 01:09:56 -0300

    Fixed input for integers

--- Ganancias.py
+++ Ganancias.py
@@ -1,14 +1,14 @@
 # -*- encoding: utf-8 -*-
 
 def Calculo(cantidad, precio):
-	total = 0
-	for x in range(0, len(cantidad)):
-		total = total + (cantidad[x] * precio[x])
-	return total
+  total = 0
+  for x in range(0, len(cantidad)):
+    total = total + (cantidad[x] * precio[x])
+  return total
 
 def Porcentaje(precio_compra, precio_venta):
-	valor = ((precio_venta-precio_compra)*100)/precio_compra
-	return valor
+  valor = ((precio_venta-precio_compra)*100)/precio_compra
+  return valor
 
 productos = []
 cantidad = []
@@ -18,19 +18,19 @@ precio_venta = []
 print ('*************** Ingreso de Datos ***************')
 opcion = 's'
 while opcion != 'n':
-	valor = input("Nombre del producto: ")
-	productos.append(valor)
+  valor = input("Nombre del producto: ")
+  productos.append(valor)
 
-	valor = input("Cantidad del producto: ")
-	cantidad.append(valor)
+  valor = int(input("Cantidad del producto: "))
+  cantidad.append(valor)
 
-	valor = input("Precio unitario del producto: ")
-	precio_compra.append(valor)
+  valor = int(input("Precio unitario del producto: "))
+  precio_compra.append(valor)
 
-	valor = input("Precio de venta del producto: ")
-	precio_venta.append(valor)
+  valor = int(input("Precio de venta del producto: "))
+  precio_venta.append(valor)
 
-	opcion = input("¿Desea ingresar otro producto? (S/n): ")
+  opcion = input("¿Desea ingresar otro producto? (S/n): ")
 
 inversion = Calculo(cantidad, precio_compra)
 ganancia_total = int(Calculo(cantidad, precio_venta))
@@ -40,7 +40,7 @@ ganancia_neta = int(ganancia_total - inversion)
 print ('*************** Resultados del Negocio ***************')
 
 for x in range(0, len(productos)):
-	print ('1)',productos[x]+':',Porcentaje(precio_compra[x], precio_venta[x]), '% de ganancia')
+  print ('1)',productos[x]+':',Porcentaje(precio_compra[x], precio_venta[x]), '% de ganancia')
 
 print ('Ganancia Total: ',ganancia_total)
 print ('Ganancia Netal: ',ganancia_neta)
